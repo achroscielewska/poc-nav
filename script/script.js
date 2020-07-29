34245162c444f21ccc5f70be3984af0aabdff29b
Chróścielewska <chroagni@DO-DAT.INT>
2020-07-29 19:17:17 +0200

    jquery to js

--- script/script.js
+++ script/script.js
@@ -1,40 +1,47 @@
-    $("#nav-1 a").on("mouseover", function() {
-    var position = $(this).position();
-    var width = $(this).parent().width();
-    
-    // https://www.w3schools.com/jquery/jquery_css_classes.asp
-    $("#nav-1 .slide2").css({ opacity: 1, left: +position.left, width: width }).addClass("squeeze");
-  });
-  
-  $("#nav-1 a").on("mouseout", function() {
-    $("#nav-1 .slide2").css({ opacity: 0 }).removeClass("squeeze");
-  });
-  
-  // // https://www.w3schools.com/jquery/traversing_find.asp
-  // var currentWidth = $("#nav-1")
-  //   .find("li:nth-of-type(3) a")
-  //   .parent("li")
-  //   .width();
-  // var current = $("li:nth-of-type(3) a").position();
+var nav = document.getElementById("nav-1");
+var hrefElements = nav.getElementsByTagName("a");
+var numHrefElements = hrefElements.length;
 
-  // $("#nav-1 .slide1").css({ left: +current.left, width: currentWidth });
+var slide1 = document.getElementsByClassName("slide1")[0];
+var slide2 = document.getElementsByClassName("slide2")[0];
 
-  var nav = document.getElementById("nav-1");
-  var hrefElements = nav.getElementsByTagName("a");
-  var numHrefElements = hrefElements.length;
-  
-  function selectElement() {
-    // https://www.w3schools.com/jsref/met_element_getboundingclientrect.asp
-    var position = this.getBoundingClientRect();
-    var slide1 = document.getElementsByClassName("slide1");
+var firstElement = document.getElementById("firstElement");
+var firstElementPosition = firstElement.getBoundingClientRect();
+slide1.style.opacity = "1";
+slide1.style.left = `${this.offsetLeft}px`;
+slide1.style.width = `${firstElementPosition.width}px`;
 
-    slide1[0].style.opacity = "1";
-    slide1[0].style.left = `${this.offsetLeft}px`;
-    slide1[0].style.width = `${position.width}px`;
-  }
+function clickElement() {
+  // https://www.w3schools.com/jsref/met_element_getboundingclientrect.asp
+  var position = this.getBoundingClientRect();
 
-  for (var i = 0; i < numHrefElements; i++) {
-    hrefElements[i].addEventListener('click', selectElement, false);
-  }
+  slide1.style.opacity = "1";
+  slide1.style.left = `${this.offsetLeft}px`;
+  slide1.style.width = `${position.width}px`;
+}
 
-  
+function mouseoverElement() {
+  var position = this.getBoundingClientRect();
+
+  slide2.style.opacity = "1";
+  slide2.style.left = `${this.offsetLeft}px`;
+  slide2.style.width = `${position.width}px`;
+  slide2.classList.add("squeeze");
+}
+
+function mouseoutElement() {
+  slide2.style.opacity = "0";
+  slide2.classList.remove("squeeze");
+}
+
+for (var i = 0; i < numHrefElements; i++) {
+  hrefElements[i].addEventListener("click", clickElement, false);
+}
+
+for (var i = 0; i < numHrefElements; i++) {
+  hrefElements[i].addEventListener("mouseover", mouseoverElement, false);
+}
+
+for (var i = 0; i < numHrefElements; i++) {
+  hrefElements[i].addEventListener("mouseout", mouseoutElement, false);
+}
